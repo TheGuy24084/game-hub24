4446d0416d51d1e6522c51bcdc8eda056a8c4244
The Guy <thgme2001@gmail.com>
2023-06-09 17:28:04 +0530

    Fetch game details

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -24,5 +24,10 @@ class ApiClient<T> {
       .get<FetchResponse<T>>(this.endpoint, config)
       .then((res) => res.data);
   };
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + "/" + id)
+      .then((res) => res.data);
+  };
 }
 export default ApiClient;
